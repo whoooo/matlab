f0d43763fb2260b4517ddb7fcb5b4a9b4376c00e
jtobin1 <jtobin@vols.utk.edu>
2015-10-28 15:22:49 -0400

    initialization of detection counts

--- xcorr_ctrl_w_indices.m
+++ xcorr_ctrl_w_indices.m
@@ -52,6 +52,9 @@ fwrite(s, cmd_config, 'uint8'); % send configuration command before sending run/
 fwrite(s, cmd, 'uint8');
 
 index = 0;
+n_detections = 0;
+n_detections_total = 0;
+event_match_indices = 0;
 if run == '1' 
     index = index + 1;
     n_detections(index) = fread(s, 1, 'int16');
